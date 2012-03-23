95351c2274e682594cdf8113d5048efc172a0575
Eneko <nkogear@gmail.com>
2012-03-23 14:45:13 +0100

    Bug correction in ActionSequence, now concurrent executions work properly.

--- src/gail/grid/executors/ActionSequence.java
+++ src/gail/grid/executors/ActionSequence.java
@@ -68,7 +68,8 @@ public class ActionSequence {
     public void execute(final GridElement targetElem, final String actionName) {
         executor.submit(new Runnable() {
             public void run() {
-                Animation actionAnimation = targetElem.getActionAnimation(actionName);
+                Animation actionAnimation =
+                                       targetElem.getActionAnimation(actionName);
                 Point finalPosition = actionAnimation.animate(targetElem);
                 if (finalPosition != null)
                     targetElem.setPositionOnGrid(finalPosition);
@@ -86,30 +87,59 @@ public class ActionSequence {
      * time, because it will be the one for which this sequence will wait until
      * it ends to continue.
      * 
+     * The GridElements must be attached (added) to a Grid.
+     * 
+     * @param targetElem
+     * @param actionName 
+     */
+    public void executeConcurrent(final Action... actions) {
+        Runnable concurrentSubmission = new Runnable() {
+            public void run() {
+                execute(actions[0].gridElement, actions[0].actionName);
+                for(int i = 1; i < actions.length; i++) {
+                    final GridElement targetElem = actions[i].gridElement;
+                    final String actionName = actions[i].actionName;
+                    Runnable runnableAction = new Runnable() {
+                        public void run() {
+                            Animation actionAnimation = 
+                                       targetElem.getActionAnimation(actionName);
+                            Point finalPosition = actionAnimation
+                                                            .animate(targetElem);
+                            if (finalPosition != null)
+                                targetElem.setPositionOnGrid(finalPosition);
+                            actionAnimation.await();
+                            if (finalPosition == null)
+                                targetElem.getGrid().remove(targetElem);
+                        }
+                    };
+                    new Thread(runnableAction).start();
+                }                
+            }
+        };
+        executor.submit(concurrentSubmission);
+    }
+    
+    /**
+     * Concurrently execute this actions on the given element into this sequence.
+     * 
+     * The first action parameter should be the one whose animation takes most
+     * time, because it will be the one for which this sequence will wait until
+     * it ends to continue.
+     * 
      * The GridElement must be attached (added) to a Grid.
      * 
      * @param targetElem
      * @param actionName 
      */
-    public void executeConcurrent(Action... actions) {
-        execute(actions[0].gridElement, actions[0].actionName);
-        for(int i = 1; i < actions.length; i++) {
-            final GridElement targetElem = actions[i].gridElement;
-            final String actionName = actions[i].actionName;
-            Runnable runnableAction = new Runnable() {
-                public void run() {
-                    Animation actionAnimation = targetElem
-                                                .getActionAnimation(actionName);
-                    Point finalPosition = actionAnimation.animate(targetElem);
-                    if (finalPosition != null)
-                        targetElem.setPositionOnGrid(finalPosition);
-                    actionAnimation.await();
-                    if (finalPosition == null)
-                        targetElem.getGrid().remove(targetElem);
-                }
-            };
-            new Thread(runnableAction).start();
+    public void executeConcurrent(final GridElement targetElem,
+                                  final String... actionNames) {
+        Action[] actions = new Action[actionNames.length];
+        int index = 0;
+        for(String str : actionNames) {
+            actions[index] = new Action(targetElem, str);
+            index++;
         }
+        executeConcurrent(actions);
     }
     
     public class Action {
